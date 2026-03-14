5bd0bffe7422f1fb26bdd844b793e3a8cbd7c7bd
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-14 15:43:35 +0600

    Add initial Models

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Fitness_Tracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/user.cs
@@ -0,0 +1,16 @@
+﻿namespace Fitness_Tracker.Models;
+
+public class user
+{
+    public string name { get; set; }
+    public string gender { get; set; }
+    public int age { get; set; }
+    public int height { get; set; }
+    public string MedicalIssue { get; set;}
+    public DateOnly ProgramStart { get; set; }
+    public DateOnly ProgramEnd { get; set; }
+    public string DietType { get; set; }
+    public int BMR { get; set; }
+    public int TotalCals { get; set; }
+    
+}
