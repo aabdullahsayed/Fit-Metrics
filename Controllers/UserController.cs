5a3eb5eff463c43987f95e86d0084c3fa6015242
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-14 22:29:30 +0600

    Add UserController

---
+++ Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using FitnessTracker.Data;
+using FitnessTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace FitnessTracker.Controllers;
+
+public class UserController:Controller
+{
+    public readonly AppDbContext _context;
+
+    public UserController(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    
+    public IActionResult Index()
+    {
+        var users = _context.users.ToList();
+        return View(users);
+    }
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Create(User user)
+    {
+        // if (ModelState.IsValid)
+        // {
+        //     _context.Add(user);
+        //     _context.SaveChanges();
+        //     return RedirectToAction(nameof(Index));
+        // }
+        
+        _context.Add(user);
+        _context.SaveChanges();
+    return RedirectToAction(nameof(Index));
+
+        //return View(user);
+    }
+    
+}
