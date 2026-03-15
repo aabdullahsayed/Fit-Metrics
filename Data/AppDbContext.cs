b822b0b0cd2bc1b34cae6fcda4e35b9a04ba2ed5
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-15 19:59:09 +0600

    Implement DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FitnessTracker.Models;
+namespace FitnessTracker.Data;
+
+public class AppDbContext:DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<User> users { get; set;}
+    public DbSet<Report> reports { get; set;}
+}
