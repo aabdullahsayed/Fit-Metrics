633b3952174ef928cb1584eb2ae8290ab122a590
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-15 19:58:35 +0600

    Add ReportController.cs

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,38 @@
+﻿using FitnessTracker.Data;
+using FitnessTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Fitness_Tracker.Controllers;
+
+public class ReportController:Controller
+{
+    public readonly AppDbContext _context;
+
+    public ReportController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var report = _context.reports;
+
+        return View(report);
+    }
+
+[HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Report report)
+    {
+        _context.Add(report);
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+}
